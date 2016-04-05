2c90e84993e4a9ff38d60b4c80caad2d1de60530
danielbarryroche <daniel.roche@lirmm.fr>
2016-04-05 14:19:46 +0200

    Added files via upload

---
+++ proLigContacts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#You will need to edit these paths to match your installation - see README_proLigContacts
+export JAVA_HOME=/home/Liam/jdk1.5.0_06/
+$JAVA_HOME/bin/java -jar proLigContacts.jar $1
